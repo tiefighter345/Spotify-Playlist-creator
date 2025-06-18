13bee30e26f542a1812ff58ea35d535f0ae5ad75
Thomas Grenet <tommygrenet@gmail.com>
2025-06-18 11:15:47 +0000

    server backend fix 5

--- server.js
+++ server.js
@@ -24,4 +24,5 @@ app.post('/exchange_token', async (req, res) => {
   res.json(data);
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
